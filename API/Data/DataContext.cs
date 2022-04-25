f675d8728814b5f1920ecf29eacd4ff67a19f4aa
kuldeep <kuldeepvatsal@gmail.com>
2022-04-25 13:19:01 +0530

    committed end of section2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using API.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+        //change the name of table
+        protected override void OnModelCreating(ModelBuilder  builder)
+        {
+          builder.Entity<AppUser>().ToTable("AppUser");
+        }
+    }
+}
