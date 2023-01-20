6c0dc6cb9e1df3dbf45005ab1a7d1ba2436b123f
kuldeep <kuldeepvatsal@gmail.com>
2023-01-20 07:23:20 +0530

    changes done for chapter  5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,12 +11,18 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
